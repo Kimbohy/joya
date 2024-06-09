8ed1613c8a3a43bbb51b8377332e7b6c6771dbc6
kimbohy <lovatina123@gmail.com>
2024-06-09 23:09:53 +0300

    add img clickable

--- assets/script/productHub.js
+++ assets/script/productHub.js
@@ -26,10 +26,10 @@ const addProduct = (produits, container) => {
     container.innerHTML = '';
     produits.forEach(produit => {
         let url = window.location.href.includes('index.html') ? "./assets/" + produit.imageUrl : "../" + produit.imageUrl;
-        console.log(url);
+        console.log(produit);
         container.innerHTML += `
         <div class="nouveaute">
-            <div class="photo">
+            <div class="photo"  onclick="imageClick('${url}','${produit.name}','${produit.prix}')">
                 <img src="${url}" alt="">
             </div>
             <div class="details_produit">
@@ -64,4 +64,56 @@ window.onload = () => {
         let container = document.querySelector(".article");
         fetchProduct(localStorage.getItem('selectedPage'), container);
     }
+}
+
+function imageClick(imageUrl, _name, prix) {
+    container = document.querySelector('.article');
+    if (container == null) {
+        container = document.querySelector('.block_nouveaute');
+    }
+    console.log("clicked");
+    container.innerHTML = `
+    <div class="new">
+    <div class="infoprod">
+        <div class="imgProd">
+            <img src="${imageUrl}" alt="">
+        </div>
+        <div class="prodInfo">
+            <div class="titreETprix">
+                <h1>${_name}</h1> <br>
+                <h2 id="price">${prix} € </h2>
+            </div>
+            <div class="ajoutPanier">
+                <span>size</span>
+                <select id="size">
+                    <option value="S">S</option>
+                    <option value="M">M</option>
+                    <option value="L">L</option>
+                </select> <br><br>
+                <span>Quantite</span>
+                <input id="quantite" type="number" value="1" min="1"> <br><br>
+                <button id="ajout">AJOUTER AU PANIER</button>
+            </div>
+        </div>
+    </div>
+    <ul>
+        <li>
+            <img src="./../img/rea_customer_service.png" alt="" class="miniIcon">
+            <span>Service  client au 06 54 94 19</span>
+        </li>
+        <li>
+            <img src="./../img/reassurance-joya-le-store-moyens-paiements.png" alt=""class="miniIcon">
+            <span>Paiement sécurisé &3x sans frais avec Aima</span>
+        </li>
+        <li>
+            <img src="./../img/rea_pdt_shipping.png" alt="" class="miniIcon">
+            <span>Livraison offerte avec Mondial Relay à partir de 100 €</span>
+        </li>
+        <li>
+            <img src="./../img/rea_click_collect.png" alt="" class="miniIcon">
+            <span>Click et collect à la Grande Motte</span>
+        </li>
+    </ul>
+    </div>
+    `;
 }
